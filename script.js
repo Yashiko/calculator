00f5b98db1d961c0263ee335a3e0d0f61568933c
Yashiko <andriusgel@gmail.com>
2018-04-17 23:29:52 +0300

    Padaryta f-ja C

--- script.js
+++ script.js
@@ -5,10 +5,16 @@
 //get all keys
 let keys = document.querySelectorAll(".buttons-body button");
 for(let i = 0; i < keys.length; i++){
-    keys[i].onclick = function main(){
-       let btnValue = this.getAttribute("data-value"); //paspausto mygtuko reiksme
-        console.log(btnValue);
+    keys[i].onclick = function(e){
+        let btnValue = this.getAttribute("data-value"); //paspausto mygtuko reiksme
+        
+        let input = document.getElementById("calcDisplay");
 
+        if (btnValue == "C"){
+            input.innerHTML = "";
+        }else{
+            input.innerHTML = btnValue;
+        }
 
     }
 
