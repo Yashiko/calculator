f4eb0549f9eb60f1b2240c4cffa88caf3b25a37e
Yashiko <andriusgel@gmail.com>
2018-04-17 21:25:04 +0300

    HTML, prideta C

--- script.js
+++ script.js
@@ -3,13 +3,13 @@
 "use strict";
 
 //get all keys
-let keys = document.querySelectorAll("#buttons button");
-
-
+let keys = document.querySelectorAll(".buttons-body button");
 for(let i = 0; i < keys.length; i++){
-    keys[i].onclick = function(e){
+    keys[i].onclick = function main(){
        let btnValue = this.getAttribute("data-value"); //paspausto mygtuko reiksme
         console.log(btnValue);
-        
+
+
     }
+
 }
