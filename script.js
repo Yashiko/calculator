189c5bd55e1240e7e5c551be3de4f97d1c3d9056
Yashiko <andriusgel@gmail.com>
2018-04-17 19:38:48 +0300

    Prideta HTML, Script

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-$(ducument).ready(function(){
+//paspaudus mygtuka jo reiksme prilyginam a reiksmei, tada spaudziame
+// veiksma ir atra reiksme prilyginam b reiksmei ir paspaudus = suskaiciuojame
+"use strict"
 
-});
+
+main();
+function main() {
+    document.getElementById("buttons").onclick = numbers;
+}
+function numbers(a, b, op) {
+    let num1 = a.target.id;
+    let num2 = b.target.id;
+
+    if (a.target.tagName == 'BUTTON') {
+        document.getElementById("calcDisplay").innerHTML = num1;
+    }else if (b.target.tagName == 'BUTTON') {
+        document.getElementById("calcDisplay").innerHTML = num2;
+    };
+}
