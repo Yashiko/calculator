2be209afac31bebd13f2d7c665da7caa9ba46205
Yashiko <andriusgel@gmail.com>
2018-04-19 15:20:48 +0300

    banymas

--- script.js
+++ script.js
@@ -4,7 +4,6 @@
 
 //get all keys
 let keys = document.querySelectorAll(".buttons-body button");
-// var operators = ['+', '-', '*', '/'];
 
 //loop through all keys
 for(let i = 0; i < keys.length; i++){
@@ -21,25 +20,28 @@ for(let i = 0; i < keys.length; i++){
             inputDisplay.innerHTML += btnValue;
         }
 
-
-
-
-
-        // if(btnValue == "="){
-        //     let result = "suma";
-        //     inputDisplay.innerHTML = result;
-
-        // }
-    
-    }
-    let operators = document.querySelectorAll(".operator");
-    let operLen = operators.length;
-    for(let y = 0; y < operLen; y++){
-        operLen[y].onclick = function(x){
+        let operators = document.querySelectorAll(".operator");
+        let operLen = operators.length;
+        for(let y = 0; y < operLen; y++){
+            operators[y].addEventListener("click",function() {
+                let op = this.value; //paspausto mygtuko reiksme
+                if(inputDisplay.innerHTML === "") {
             
+                    inputDisplay.innerHTML = inputDisplay.innerHTML.concat("");
+                    
+                }else if(btnValue) {
+                    inputDisplay.innerHTML = btnValue.concat(op);
+                    
+                }
+                   
+            })
         }
+
+
     }
+
 }
+
 // switch(btnValue){
 //     case '*':
 //         let result = btnValue*btnValue;
