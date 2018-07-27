f448b333438dba4d995fbccb0fc793c099a4a985
Andrius <andriusgel@gmail.com>
2018-07-27 23:22:43 +0300

    Tesiamas darbas
    Kolkas nepavysta

--- script.js
+++ script.js
@@ -13,17 +13,16 @@ for(let i = 0; i < keys.length; i++){
         let inputDisplay = document.getElementById("calcDisplay");
         let btnValue = this.getAttribute("data-value"); //paspausto mygtuko reiksme
         let operatorValue = this.getAttribute("value"); //oeratoriaus reiksme
-
         if (btnValue == "C"){  
             inputDisplay.innerHTML = " ";
 
         }else if(inputDisplay.innerHTML === ""){
             inputDisplay.innerHTML = btnValue;
-            let num1 = btnValue;
 
-        }else if(operatorValue){   //jei skaicius yra is dvieju zenklu
+        }else if(btnValue == "*"){   //jei skaicius yra is dvieju zenklu
             // btnValue = inputDisplay.innerHTML += btnValue; // dvizenklis skaicius
-            inputDisplay.innerHTML = btnValue + btnValue;
+            inputDisplay.innerHTML = "2";
+            // console.log(btnValue);
 
         };
 
