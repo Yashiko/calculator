ff48e29849bdcd08ece5338c2a1e0b8cf0581a5b
Andrius <andriusgel@gmail.com>
2018-07-28 08:33:40 +0300

    darbas
    Vis dar mėginama padaryti skirtuma tarp operandu  ir skaičių

--- script.js
+++ script.js
@@ -4,47 +4,36 @@
 //Naujas Projekto
 //get all keys
 let keys = document.querySelectorAll(".buttons-body button");
-// let operators = document.querySelectorAll(".operator");
+let operators = document.querySelectorAll(".operator");
  
 //loop through all keys
 for(let i = 0; i < keys.length; i++){
-
     keys[i].onclick = function(e){
         let inputDisplay = document.getElementById("calcDisplay");
         let btnValue = this.getAttribute("data-value"); //paspausto mygtuko reiksme
-        let operatorValue = this.getAttribute("value"); //oeratoriaus reiksme
         if (btnValue == "C"){  
             inputDisplay.innerHTML = " ";
 
         }else if(inputDisplay.innerHTML === ""){
             inputDisplay.innerHTML = btnValue;
 
-        }else if(btnValue == "*"){   //jei skaicius yra is dvieju zenklu
-            // btnValue = inputDisplay.innerHTML += btnValue; // dvizenklis skaicius
-            inputDisplay.innerHTML = "2";
-            // console.log(btnValue);
-
         };
 
-
-    
-        // for(let y = 0; y < operators.length; y++){
-        //     operators[y].addEventListener("click",function() {
-        //         if(inputDisplay.innerHTML === "") {
-            
-        //             inputDisplay.innerHTML = btnValue+btnValue;
-                    
-        //         }else if(btnValue) {
-        //             inputDisplay.innerHTML = btnValue;
-                    
-        //         }
-                   
-        //     })
-        // }
-
-
     }
-
+}
+for(let y = 0; y < operators.length; y++){
+    let operatorValue = this.getAttribute("value"); //oeratoriaus reiksme
+    operators[y].addEventListener("click",function() {
+        if(operatorValue) {
+    
+            inputDisplay.innerHTML = operatorValue;
+            
+        }else {
+            inputDisplay.innerHTML =" btnValue";
+            
+        }
+           
+    })
 }
 
 function add(){
