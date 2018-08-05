96f56c84cbdd646936d18430191457dfc3142db6
Andrius <andriusgel@gmail.com>
2018-08-05 12:22:45 +0300

    Nauja ideja

--- script.js
+++ script.js
@@ -1,58 +1,20 @@
 
 "use strict";
 
-let keys = document.querySelectorAll(".buttons-body button");
-let operators = ['+', '-', 'x', '÷'];
 
-//loop through all keys
-for (let i = 0; i < keys.length; i++) {
-    keys[i].onclick = function (e) {
-        let ats;
         let inputDisplay = document.getElementById("calcDisplay");
-        inputDisplay.innerHTML = this.value;
-        let inputValue = inputDisplay.innerHTML;
-        console.log(inputValue);
         
-       if (this.value === "C"){  
-            inputDisplay.innerHTML = " "; 
+        function pushBtn(obj){
+            let push = obj.innerHTML;
+
+            if (push == "="){
+                //calculate
+                inputDisplay.innerHTML = eval(inputDisplay.innerHTML);
+            }else if(push == "C"){
+                //All clear
+                inputDisplay.innerHTML = '0';
+                inputDisplay.innerHTML = push;
+            } else {
+                inputDisplay.innerHTML += push
+            }
         }
-        if (this.value == "+") {
-            let btnVal = this.getAttribute("value");
-            // num2 = this.value; //paspausto mygtuko reiksme
-           ats = Number(inputValue) + Number(btnVal);
-
-        } else if (this.value == "*") {
-            num2 = this.getAttribute("value"); //paspausto mygtuko reiksme
-            ats = Number(num1) * Number(num2);
-        } else if (this.value == "/") {
-            num2 = this.getAttribute("value"); //paspausto mygtuko reiksme
-            ats = Number(num1) / Number(num2);
-        } else if (this.value == "-") {
-            num2 = this.getAttribute("value"); //paspausto mygtuko reiksme
-            ats = Number(num1) - Number(num2);
-        };
-        
-        if ( this.value == "=") {
-            inputDisplay.innerHTML = ats; // 'this' is the button that gets clicked. We add its value to the input field
-        }
-
-
-    }
-}
-
-            // let operatorValue = this.getAttribute("value"); //oeratoriaus reiksme
-        // let btnValue2 = this.getAttribute("data-value");
-
-        // for(let y = 0; y < operators.length; y++){
-    //     let operatorValue = this.getAttribute("value"); //oeratoriaus reiksme
-    //     operators[y].addEventListener("click",function() {
-    //         if(operatorValue) {
-
-    //             inputDisplay.innerHTML = operatorValue;
-
-    //         }else {
-    //             inputDisplay.innerHTML =" btnValue";
-
-    //         }
-
-    //     });f
