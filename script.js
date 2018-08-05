90454919038a178409d2fe3d653506b148ee295a
Andrius <andriusgel@gmail.com>
2018-08-05 16:54:24 +0300

    display ilgis sutvarkytas
    Projektas baigtas, veliau nebent tobulint

--- script.js
+++ script.js
@@ -7,18 +7,15 @@ let inputDisplay = document.getElementById("calcDisplay");
 function pushBtn(obj) {
     let value = obj.innerHTML;
     if (value == "=") {
-        
         //calculate values
         let result = eval(inputDisplay.innerHTML);
-        let b = result.length;
-        console.log(b);
-        if (result.length >= 11){
-            
-            inputDisplay.innerHTML = result.tofixed(11);
+        // display values length
+        if (result.toString().length > 10) {
+            //show result only 10 simbols after the dot
+            inputDisplay.innerHTML = result.toFixed(10);
         } else {
             inputDisplay.innerHTML = result;
         }
-        
     } else if (value == "C") {
         //clear dispay
         inputDisplay.innerHTML = ' ';
